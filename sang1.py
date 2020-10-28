9317224b37dfb015343bdcc11f3415b78da5699c
Ironside999 <71923458+Ironside999@users.noreply.github.com>
2020-10-28 15:21:07 +0330

    Add files via upload

---
+++ sang1.py
@@ -0,0 +1,26 @@
+user1=input("enter your value\t")
+user2=input("\n*\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nenter your value\t")
+if user1=="sang":
+    if user2=="sang":
+        print("Draw")
+    elif user2=="kaghaz":
+        print("user2 Won")
+    elif user2=="gheychi":
+        print("User1 Won")
+if user1=="kaghaz":
+    if user2=="kaghaz":
+        print("Draw")
+    elif user2=="gheychi":
+        print("user2 Won")
+    elif user2=="sang":
+        print("User1 Won")
+if user1=="gheychi":
+    if user2=="gheychi":
+        print("Draw")
+    elif user2=="sang":
+        print("user2 Won")
+    elif user2=="kaghaz":
+        print("User1 Won")
+
+
+   
